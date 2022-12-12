90db58f95ddc9db9360ced36338fc4ce606f788d
William's Student <williamchapronpro@gmail.com>
2022-12-12 23:59:09 +0100

    hi

---
+++ myapp/src/components/SimpleForm.js
@@ -0,0 +1,28 @@
+import { useForm } from "react-hook-form";
+
+
+
+export default function SimpleForm(props){
+
+
+    
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = data => { 
+        props.functionName(data);
+        props.refreshPage();
+    };
+
+
+    return <form onSubmit={handleSubmit(onSubmit)}>
+        
+        <input type="hidden" {...register("id", {required: true})} value={props.pokemonId}/> <br></br> <br></br>
+        <input type="submit" value={props.valueSubmit} /> 
+    </form>
+
+
+
+
+    
+
+}
